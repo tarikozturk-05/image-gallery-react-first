10ae7e97df823da6a702314a48f34d91c7d05e78
Tarik <a.tarik2005@gmail.com>
2022-10-07 21:23:25 +0300

    bittii

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import data from '../helper/data'
+import Picture from '../components/Picture'
+
+
+
+const Homepage = () => {
+  return (
+    <div>
+        <h1>Albumsler</h1>
+        <div className='pictures'>
+            {data.map((x) =>{
+                return <Picture data={x}/>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
